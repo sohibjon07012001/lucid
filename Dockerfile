b5fd8bd62acf1e1fabd291e824903595a5a5f03d
sohibjon07012001 <sohibjon07012001@gmail.com>
2023-09-15 10:14:45 +0500

    commit for render

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,15 @@
 FROM python:3.8
 
+RUN mkdir /fastapi_app
+
+WORKDIR /fastapi_app
+
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt
+
+COPY . .
+
+
+# CMD uvicorn main:app  uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- config.py
+++ config.py
@@ -2,7 +2,11 @@ import os
 from  dotenv import load_dotenv
 
 load_dotenv()
-
+DB_USER = os.getenv("DB_PASS")
+DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
 class JWT:
     ALGORITHM = "HS256"
@@ -11,7 +15,8 @@ class JWT:
 
 class Database:
     MODULES = ['db.users', 'db.partners']
-    CONNECTION_STRING = os.getenv("DB_CONNECTION_STRING")
+    # CONNECTION_STRING = os.getenv("DB_CONNECTION_STRING")
+    CONNECTION_STRING = f"postgres://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
 class AzureStorage:
